daad355ec238d1b393521673fce20e2ca80514f0
Namita Roy <Namita.Roy@tvsmotor.com>
2024-10-04 13:48:44 +0530

    builf fix

--- src/sxastarter/src/components/Award.tsx
+++ src/sxastarter/src/components/Award.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { ComponentParams, Field, ImageField, NextImage as JssImage, Text as JSSText } from '@sitecore-jss/sitecore-jss-nextjs';
+import {
+  ComponentParams,
+  Field,
+  ImageField,
+  NextImage as JssImage,
+  Text as JSSText,
+} from '@sitecore-jss/sitecore-jss-nextjs';
 
-export interface AwardFields{
+export interface AwardFields {
   Heading: Field<string>;
   Description: Field<string>;
-  Image:ImageField;
-
+  Image: ImageField;
 }
 
 export interface AwardProps {
@@ -15,22 +20,25 @@ export interface AwardProps {
 
 export const Default = (props: AwardProps): JSX.Element => {
   const modifyImageProps = {
-      ...props.fields.Image,
-      editable: props?.fields?.Image?.editable
-        ?.replace(`width="${props?.fields?.Image?.value?.width}"`, 'width="100%"')
-        .replace(`height="${props?.fields?.Image?.value?.height}"`, 'height="100%"'),
-    }
+    ...props.fields.Image,
+    editable: props?.fields?.Image?.editable
+      ?.replace(`width="${props?.fields?.Image?.value?.width}"`, 'width="100%"')
+      .replace(`height="${props?.fields?.Image?.value?.height}"`, 'height="100%"'),
+  };
 
   return (
     <div className="slick-slide slick-current slick-active">
-    <figure className="figure contentAside eqHeight">
-       <JssImage field={modifyImageProps}></JssImage>
-       <figcaption className="figure-caption">
-            <h2 className="figureTitle"><JSSText field={props.fields.Heading} /></h2>
-            <p><JSSText field={props.fields.Description} /></p>
+      <figure className="figure contentAside eqHeight">
+        <JssImage field={modifyImageProps}></JssImage>
+        <figcaption className="figure-caption">
+          <h2 className="figureTitle">
+            <JSSText field={props.fields.Heading} />
+          </h2>
+          <p>
+            <JSSText field={props.fields.Description} />
+          </p>
         </figcaption>
-    </figure>
-
-</div>
+      </figure>
+    </div>
   );
 };
--- src/sxastarter/src/components/AwardTeaser.tsx
+++ src/sxastarter/src/components/AwardTeaser.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { ComponentParams, ComponentRendering} from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentParams, ComponentRendering } from '@sitecore-jss/sitecore-jss-nextjs';
 import { AwardFields, Default as Award, AwardProps } from './Award';
 
-export interface AwardTeaserField{
-  Awards:Array<Award>;
+export interface AwardTeaserField {
+  Awards: Array<Award>;
 }
 
-export interface Award{
+export interface Award {
   fields: AwardFields;
 }
 
@@ -17,22 +17,20 @@ interface AwardTeaserProps {
 }
 
 export const Default = (props: AwardTeaserProps): JSX.Element => {
-   return (
+  return (
     <div className="layoutWrap achivements fullWidthMob">
       <div className="awardsWrap">
-          <h2 className="animatable fadeInLeft delay750">Award</h2>
-          <div className="slide4Col slick-initialized slick-slider">
-
-                  { props.fields.Awards.map((award,idx)=>{
-                    const awardProps: AwardProps = {
-                      params: props.params,
-                      fields: award.fields,
-                    };
-                    return <Award key={idx} {...awardProps}></Award>
-                  })}
-
-            </div>
-            </div>
-            </div>
+        <h2 className="animatable fadeInLeft delay750">Award</h2>
+        <div className="slide4Col slick-initialized slick-slider">
+          {props.fields.Awards.map((award, idx) => {
+            const awardProps: AwardProps = {
+              params: props.params,
+              fields: award.fields,
+            };
+            return <Award key={idx} {...awardProps}></Award>;
+          })}
+        </div>
+      </div>
+    </div>
   );
 };
